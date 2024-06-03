25528ffd4b36ba59f4f7da166b5070d8e411a78e
misaelsoousa <misasoousa@gmail.com>
2024-06-03 13:12:19 -0300

    Criação do projeto e inicio da navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,32 @@
+// import styles from './Navbar.module.css'
+
+import './navbar.scss';
+
+import { Logo, NewRating, Search } from '../assets/assets'
+function Navbar() {
+
+    return (
+        <div>
+
+            <nav className="navbar">
+                <div className="logo">
+                    <Logo/>
+                    <p><span>BUZZ</span>BEAT</p>
+                </div>
+                <div className="search">
+                    <Search/>
+                    <input type='text'></input>
+                </div>
+                    <button className="btn">
+                        
+                        <NewRating className="rating"/>
+                        
+                        Nova avaliação
+                    </button>
+
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
